045792fd6622f03e669661d2a51ab4111778db5c
Thibaud Franchetti <noreply@franchetti.ch>
2023-12-28 00:01:43 +0100

    Remove sqlite support

--- compose.sh
+++ compose.sh
@@ -8,17 +8,17 @@ else
     exit 1
 fi
 
-FILES="-f compose.seafile.common.yml -f compose.proxy.common.yml"
+FILES="-f compose.seafile.yml"
 
-if [ $DBMS -eq 1 ]
+if [ $DBMS -eq 0 ]
 then
-    FILES=$FILES" -f compose.seafile.mariadb.yml -f compose.db.common.yml -f compose.db.mariadb.yml"
-elif [ $DBMS -eq 2 ]
+    FILES="$FILES -f compose.db.mariadb.yml"
+elif [ $DBMS -eq 1 ]
 then
-    FILES=$FILES" -f compose.seafile.mariadb.yml -f compose.db.common.yml -f compose.db.mysql.yml"
+    FILES="$FILES -f compose.db.mysql.yml"
 else
-    FILES=$FILES" -f compose.seafile.sqlite.yml"
-    export SQLITE=1
+    echo "Unsupported DBMS"
+    exit 1
 fi
 
 if [ $NOSWAG -eq 1 ]
